6087aed01c9f352c0477f9d8aadba94e224e4b3f
williamjayjay <williamdn@yahoo.com>
2024-04-20 02:21:02 -0300

    feat: teste v2

--- src/screens/Dash/Chart.tsx
+++ src/screens/Dash/Chart.tsx
@@ -1,11 +1,19 @@
-import React from "react";
-import { Dimensions } from "react-native";
+import React, { useEffect } from "react";
+import { Dimensions, StyleSheet, TouchableOpacity } from "react-native";
 import { Svg } from "react-native-svg";
 import { scaleLinear } from "d3-scale";
 
 import Candle, { Candle as CandleModel } from "./Candle";
 
 export const { width: size } = Dimensions.get("window");
+
+import Animated, {
+  measure,
+  runOnJS,
+  useAnimatedRef,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 interface ChartProps {
   candles: CandleModel[];
@@ -18,14 +26,74 @@ export default ({ candles, domain }: any) => {
   const scaleBody = scaleLinear()
     .domain([0, Math.max(...domain) - Math.min(...domain)])
     .range([0, size]);
+
+
+  // ------------------
+
+  const animatedRef = useAnimatedRef();
+  const altura = useSharedValue(0);
+  const [text, setText] = React.useState(altura.value);
+
+  const measurement = measure(animatedRef);
+
+  const handlePress = () => {
+    if (altura.value >= size) {
+      return;
+    }
+    altura.value = withTiming(altura.value + size, {}, () => {
+      // highlight-next-line
+      if (measurement === null) {
+        return;
+      }
+
+
+      runOnJS(setText)(Math.floor(measurement.width));
+    });
+  };
+
+  useEffect(() => {
+
+    handlePress()
+  }, [])
+
+
+
   return (
-    <Svg width={size} height={size}>
-      {candles.map((candle: any, index: any) => (
-        <Candle
-          key={candle.date}
-          {...{ candle, index, width, scaleY, scaleBody }}
-        />
-      ))}
-    </Svg>
+    <Animated.View ref={animatedRef} style={{ ...styles.box, height: altura }} >
+
+
+      <Svg width={size} height={size}>
+        {candles.map((candle: any, index: any) => (
+          // <TouchableOpacity key={candle.date} onPress={() => console.log('testecandle', candle.date)} >
+
+          <Candle
+            key={candle.date}
+            {...{ candle, index, width, scaleY, scaleBody }}
+          />
+          // </TouchableOpacity>
+        ))}
+      </Svg>
+    </Animated.View>
+
   );
 };
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: "center"
+  },
+  box: {
+    height: 100,
+    // width: 400,
+
+  },
+  label: {
+    fontSize: 24,
+    marginVertical: 16,
+    color: '#b58df1',
+  },
+});
--- src/screens/Dash/index.tsx
+++ src/screens/Dash/index.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Chart, { size } from "./Chart";
 import data from "./data.json";
 
 
 // import { onGestureEvent, useValues } from 'react-native-redash';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+import Animated from 'react-native-reanimated';
+
 // import { State } from 'react-native-gesture-handler';
 
 
 export function Dash() {
 
-
   // const [x, y, state] = useValues(0, 0, State.UNDETERMINED);
 
 
+  // const gestureHandler = onGestureEvent({
+  //   x,
+  //   y,
+  //   state,
+  // });
 
-  const candles = data.slice(0, 20);
+
+  // const [x, y, state] = useValues(0, 0, State.UNDETERMINED);
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "black",
+    },
+  });
+
+  const candles = data.slice(0, 10);
 
   const getDomain = (rows: any) => {
     const values = rows.map(({ high, low }: any) => [high, low]).flat();
@@ -30,6 +47,37 @@ export function Dash() {
 
 
       <Chart {...{ candles, domain }} />
+
+      {/* <Animated.View style={{}} >
+
+
+     </Animated.View> */}
+
+
+      {/* <PanGestureHandler minDist={0} > */}
+
+      {/* <Animated.View style={StyleSheet.absoluteFill}> */}
+      {/* <Animated.View
+              style={{
+                transform: [{ translateY }],
+                opacity,
+                ...StyleSheet.absoluteFillObject,
+              }}
+            >
+              <Line x={size} y={0} />
+            </Animated.View>
+            <Animated.View
+              style={{
+                transform: [{ translateX }],
+                opacity,
+                ...StyleSheet.absoluteFillObject,
+              }}
+            >
+              <Line x={0} y={size} />
+            </Animated.View>
+            <Label y={translateY} {...{ size, domain, opacity }} /> */}
+      {/* </Animated.View>
+      </PanGestureHandler> */}
     </View>
   );
 }
